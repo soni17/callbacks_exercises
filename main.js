648bace6689785df4af59a8d8f9164d3011f550e
soni17 <elb004@gmail.com>
2017-11-10 16:30:23 -0500

    2-calculate the number of purchases

--- main.js
+++ main.js
@@ -164,7 +164,12 @@ console.log( 'The total number of sales is:', numSales );
 /*
   Calculate the total number of 'purchases'.
 */
-var numPurchases;
+
+var purchases = transactions.filter( function(transaction){
+  return transaction.type == 'purchase';
+});
+
+var numPurchases = purchases.length;
 
 console.log( 'The total number of purchases is:', numPurchases );
 
