0dc352a66a963cf81555b089a84b3b52f2c18c48
soni17 <elb004@gmail.com>
2017-11-10 16:29:49 -0500

    1-calculate the number of sales

--- main.js
+++ main.js
@@ -129,7 +129,12 @@ console.log( 'The total number of transactions is:', totalTransactions );
   HINT(S):
   - Not all transactions are 'sales'.
 */
-var numSales;
+
+var sales = transactions.filter( function(transaction){
+  return transaction.type == 'sale';
+});
+
+var numSales = sales.length;
 
 /*
   Hey, welcome to the first question!
